cf942cf916cbee7f4eea992d949d6eac51a2216b
KevalKThumar <thumarkeval12@gmail.com>
2024-07-13 23:58:07 +0530

    first commit

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
     console.log('listening on *:3000');
 });  
